f4574a3e15a98b1a9c5050265d54ae0a510588f1
NICHOLAS-1217 <nicholastan1217@gmail.com>
2023-12-27 02:36:25 +0800

    alert messages for signup

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["get", "post"])
 def login():
-    return "login"
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
     return "logout"
 
-@auth.route("/signup")
+@auth.route("/signup", methods=["GET", "POST"])
 def signup():
-    return "signup"
+    data = request.form
+    print(data)
+    if request.method == "POST":
+        email = request.form.get("email")
+        name = request.form.get("name")
+        password = request.form.get("password")
+        confirmPassword = request.form.get("confirmPassword")
+        if len(email) < 4:
+            flash("email must be greater than 3 characters", category="error")
+        elif len(name) < 2:
+            flash("name must be greater than 1 characters", category="error")
+        elif len(password) < 7:
+            flash("password dont\'t match", category="error")
+        elif password != confirmPassword:
+            flash("password too short", category="error")
+        else:
+            flash("account created", category="success")
+    return render_template("signup.html")
