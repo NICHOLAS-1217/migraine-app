ee2c852c3f5644f17fb87ccfa4e4e57562b2f80a
NICHOLAS-1217 <nicholastan1217@gmail.com>
2024-01-07 06:14:34 +0800

    patient status form done

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+import flask
 from .models import User, Caretaker
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
@@ -7,9 +7,11 @@ views = Blueprint("views", __name__)
 def welcome():
     return render_template("welcome.html")
 
-@views.route("/home")
+@views.route("/home", methods=["GET", "POST"])
 @login_required
 def home():
+    data = request.form
+    print(data)
     return render_template("home.html", user=current_user)
 
 @views.route("/migraineHistory")
